5cd023500bfde45ca427da4ccfa3bce7b39d7847
Godson Sakawa <godsonsakawa@gmail.com>
2022-10-15 18:12:29 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /*
- * main - entry point function
+ * main - Entry point
  *
  * puts() ----- outputs characters and strings
  *
  * return ------Return: Always 0 (Success)
  */
- 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
