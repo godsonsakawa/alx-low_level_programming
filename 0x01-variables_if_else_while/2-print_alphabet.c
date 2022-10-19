59bcc2049d9fe8c35c159efbe1f63e40a10aedfd
Godson Sakawa <godsonsakawa@gmail.com>
2022-10-19 02:28:58 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+		putchar('\n');
+	}
+
+	return (0);
+}
+
