f3d9264a33a4cef9c14fee7eccf766de954732bc
Godson Sakawa <godsonsakawa@gmail.com>
2022-10-23 13:25:01 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two intergers
+ * @x: an integer to add to y
+ * @y: an integer to add to x
+ * Return: void.
+ */
+int add(int x, int y)
+{
+	int add;
+
+	add = x + y;
+	return (add);
+}
