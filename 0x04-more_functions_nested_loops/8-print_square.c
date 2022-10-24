69d1329625ac1052114a7bd8e368d886ded1a7aa
Godson Sakawa <godsonsakawa@gmail.com>
2022-10-24 11:26:26 -0700

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - prints a square.
+ * @n: integer that references #
+ * Return: void.
+ */
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
