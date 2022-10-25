54d902acfd2537327836a53da324846dcd45541e
Godson Sakawa <godsonsakawa@gmail.com>
2022-10-25 12:17:49 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers.
+ * @a: references the first integer.
+ * @b: references the second integer.
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
