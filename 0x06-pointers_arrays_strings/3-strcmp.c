81012004b07dc31d94da5c79626c6b00405a355c
root <root@HP-Folio.localdomain>
2022-10-30 19:38:17 +0300

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,18 +2,21 @@
 #include <string.h>
 /**
  * _strcmp - compare two strings
- * @*s1: references the first string.
- * @*s2: references the second string.
+ * @s1: references the first string.
+ * @s2: references the second string.
  * Return: 0 Always success.
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while ((s1[i] == s2[i]) || (s1[i] >= s2[i]) || (s1[i] <= s2[i]) && s1[i] != '\0'))
+	while ((s1[i] == s2[i] && s2[i] != '\0'))
 	{
+		if (s1[i] != s2[i])
+		{
+			break;
+		}
 		i++;
 	}
 	return (s1[i] - s2[i]);
 }
-
