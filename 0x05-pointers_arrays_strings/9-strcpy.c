cdc73eb09958406f43e18915724cd4ce371757fe
root <root@HP-Folio.localdomain>
2022-10-31 00:38:22 +0300

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *_strcpy - copies a string
+  *@dest:  the 1st string/destination string.
+  *@src: references the 2nd string which is copied/ source string.
+  *Return: Always 0 (success)
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len;
+
+	len = strlen(src);
+
+	for (i = 0; i < len && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < len; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
+
