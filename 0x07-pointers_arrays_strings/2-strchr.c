73869daa5b4dd329b8db46e17b2fdf237dcb1175
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-06 21:40:43 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strchr - locates character of a string.
+  * @c: The character to be located in string s.
+  * @s: The string containing the characters to be located.
+  * Return: return s.
+  */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}
