b49ba111474d2b0accd651a16fc293a189dffe09
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-16 23:46:30 +0300

    a function that initializes a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+  * init_dog - a function that initializes a variable of type struct dog
+  * @name: pointer to name of the dog
+  * @age: pointer to age of the dog
+  * @owner: pointer to the owner of the dog
+  * @d: poiter to type My_dog
+  * Return: void.
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
+
