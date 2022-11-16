39ae129a1d2ec1a5fa8d67afb1f0815fea62a28a
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-16 23:06:30 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _MY_DOG_
+#define _MY_DOG_
+
+/**
+  * struct dog - structure dog
+  * @name: pointer to name of the dog
+  * @age: age of the dog.
+  * @owner: pointer to owner of the dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
