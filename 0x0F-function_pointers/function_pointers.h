a7196c65b9f44fc5f5084f0d7b46ee6c30af0aa2
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-17 19:07:40 +0300

    Header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
-#ifndef _FUNCTION_POINTERS_
-#define _FUNCTION_POINTERS_
+#ifndef _FUNCTION_POINTER_
+#define _FUNCTION_POINTER_
 
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+
+
 
 
 #endif
