bef799e0ccc8ff3fdacdec78bf1b3861991c5de5
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-17 19:21:07 +0300

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,17 +7,7 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	void print_name_as_is(char *name);
-	void print_name_uppercase(char *name);
-
-	if (f == print_name_as_is)
-	{
-		f(name);
-	}
-	else if (f == print_name_uppercase)
-	{
-		 f(name);
-	}
+	f(name);
 }
 
 
