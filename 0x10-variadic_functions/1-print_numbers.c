5cc9a1711c1f2fc35e5024eae4726db4e8a99d5a
root <root@HP-Folio.localdomain>
2022-11-19 23:12:51 +0300

    print numbers with a separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,11 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i = 0;
 	unsigned int nums;
-	
+
 	va_start(ap, n);/* Initialize the argument list*/
 	while (i < n)
 	{
-		
 		nums = va_arg(ap, int);/* get the next argument value*/
 		printf("%d", nums);
 		if (i < (n - 1) && separator != NULL)
