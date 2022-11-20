b17b810a3ee4cae2f52ea5c74ddf86f905b85651
root <root@HP-Folio.localdomain>
2022-11-20 14:44:11 +0300

    print strings with a separator

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,10 +17,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(string, char *);
-		printf("%s", str);
-		if (i < (n - 1) && separator != NULL)
+		if (str == NULL)
 		{
-			printf("%s", separator);
+			printf("(nill)");
+		}
+		else
+		{
+			printf("%s", str);
+			if (i < (n - 1) && separator != NULL)
+			{
+				printf("%s", separator);
+			}
 		}
 	}
 	va_end(string);
