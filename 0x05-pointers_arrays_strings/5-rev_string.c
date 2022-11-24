cd6a08d512c0cab1d58f223158da8e5f8e709cb0
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-24 06:48:17 +0300

    Function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,14 @@
  */
 void rev_string(char *s)
 {
-	/*@i: references each element in an array*/
 	int i = 0;
-	/*@j: references each element from last to first*/
 	int j = 0;
-	/*@n: references the total number of elements in an array*/
 	int n;
 
 	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
 		i++;
 	}
-	/*reverse*/
 	i--;
 
 	while (i > j)
@@ -28,4 +23,3 @@ void rev_string(char *s)
 		s[j++] = n;
 	}
 }
-
