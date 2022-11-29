aa644db84125c684b76bf3b96ca0c8b533aa8403
Godson Sakawa <godsonsakawa@gmail.com>
2022-11-29 23:44:42 +0300

    Capitalize staring char of words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * cap_string - function that capitalizes all start words of a string.
+  * @s: string to be referenced.
+  * Return: Returns a pointer to a character.
+  */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 32 || s[i] == '\t' ||
+				s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+				s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+				s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
