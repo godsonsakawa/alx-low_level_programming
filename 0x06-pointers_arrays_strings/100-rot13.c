3e6c72682dd7b4b6ae1ae9af77c5de055f825bc5
Godson Sakawa <godsonsakawa@gmail.com>
2022-12-01 03:26:21 +0300

    A function that encodes using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string using rot13
+  * @s: input string
+  * Return: pointer to char.
+  */
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 26; j++)
+		{
+			if (s[i] == alphabet[j])
+				s[i] = rot13[j];
+		}
+	}
+	return (s);
+}
