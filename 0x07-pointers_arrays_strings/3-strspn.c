385dc2bedf8543c424c6978907ee0c901d6910fe
Godson Sakawa <godsonsakawa@gmail.com>
2022-12-02 13:19:38 +0300

    return length of a prefix substring that is in accept

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * _strspn - gets the length of a prefix substring.
+  * @s: prefix substring to be scanned..
+  * @accept: bytes to match on s bytes.
+  * Return: number of bytes in the prefix substring that matches what is in accept.
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int index;
+	
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (accept[index] == *s)
+			{
+				count++;
+				break;				
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (count);
+			}
+		}
+		s++;
+	}
+	return (count);
+}
