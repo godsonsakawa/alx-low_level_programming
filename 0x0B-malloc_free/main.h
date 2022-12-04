88bb753ebad8c383d34f21f2b81ef1ecd64f5141
Godson Sakawa <godsonsakawa@gmail.com>
2022-12-04 17:52:09 +0300

    Header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdlib.h>
+#include <stddef.h>
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
