3896344f5c3fa1dbe878cbc7d4ea36c315b0764c
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-16 22:23:23 +0300

    Creating a dynamic library

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strncpy
+  * @src: 
+  * @dest:
+  * @n
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
+
---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+  * _strpbrk
+  *
+  */
+char *_strpbrk(char *s, char *accept)
+{
+
+}
