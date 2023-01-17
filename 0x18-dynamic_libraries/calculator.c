c1ad30f3c9aaddfd283ca04361507ade53e20e14
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-17 03:04:51 +0300

    Calling C functions from python

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+  * add - addition.
+  * @a: first integer
+  * @b: second integer
+  * Return: Always 0 Success.
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * sub - subtraction
+  * @a: first integer
+  * @b: second integer
+  * Return: Always 0 success.
+  */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * mul - multiplication
+  * @a: first integer
+  * @b: second integer
+  * Return: Always 0 success.
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * div - division
+  * @a: first integer
+  * @b: second integer
+  * Return: Always 0 success.
+  */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * mod - modulus
+  * @a: first integer
+  * @b: second integer
+  * Return: Always 0 success.
+  */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
