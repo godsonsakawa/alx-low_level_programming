1cdb9eca19b51b28dafa15d970b1cc33ca0fa71c
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-18 05:03:37 +0300

    Task 3 Initializing structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,14 +9,20 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		;
+		return;
+	
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	else
-	{
-		if (d->name != NULL && d->age != 0 && d->owner != NULL)
-			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-		else if (d->name == NULL) 
-			printf("Name: (nil)\n");
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-	}
+		printf("Name: %s\n", d->name);
+	
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("owner: (nil)\n");
+	else
+		printf("owner: %s\n", d->owner);
 }
