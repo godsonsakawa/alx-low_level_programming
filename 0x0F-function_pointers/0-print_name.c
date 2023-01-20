5fa3b25cf94a4792eee270306d6d64169162098b
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-20 14:36:43 +0300

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
   * print_name - prints name
   * @name: name to be printed
@@ -7,7 +8,8 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
-
-
