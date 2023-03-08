117ab070885c29baa96d0792bf6fdd87a919aed1
godson sakawa <godsonsakawa@gmail.com>
2023-03-08 12:02:12 +0300

    Print every minute of the day - Jack Bauer - Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,40 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int hr1, hr2, mn1, mn2;
+	int hrs, mins;
 
-	for (hr1 = 0; hr1 <= 2; hr1++)
+	for (hrs = 0; hrs < 24; hrs++)
 	{
-		if (hr1 == 2 && hr2 == 4)
+		for (mins = 0; mins < 60; mins++)
 		{
-			break;
-		}
-		for (hr2 = 0; hr2 < 10; hr2++)
-		{
-			if (hr1 == 2 && hr2 == 4)
-			{
-				break;
-			}
-			for (mn1 = 0; mn1 < 6; mn1++)
-			{
-				if (hr1 == 2 && hr2 == 4)
-				{
-					break;
-				}
-				for (mn2 = 0; mn2 < 10; mn2++)
-				{
-					if (hr1 == 2 && hr2 == 4)
-					{
-						break;
-					}
-					_putchar(hr1 + '0');
-					_putchar(hr2 + '0');
-					_putchar(':');
-					_putchar(mn1 + '0');
-					_putchar(mn2 + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar(hrs / 10 + '0');
+			_putchar(hrs % 10 + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(mins % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
