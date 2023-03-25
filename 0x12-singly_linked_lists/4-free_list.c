b4ee87cd6498c6f9e96d2c38c1092f42f742cbbf
godson sakawa <godsonsakawa@gmail.com>
2023-03-26 01:03:26 +0300

    Fix Memory leak:  Freeing memory of a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,13 +3,10 @@
 
 /**
  * free_list - Frees a listint_t list.
- * @head: A pointer to the head of the listint list to be freed.
+ * @head: A pointer to the head of the list to be freed.
  *
  * Description: To free memory of a linked list, we need to traverse the list
- *		and free each node one by one. For each node in the list,
- *		we store the current node in a temporary pointer variable (temp),
- *		move to the next node using the next pointer,
- *		and free the memory for the node stored in temp.
+ *		and free each node one by one.
  */
 void free_list(list_t *head)
 {
@@ -20,6 +17,7 @@ void free_list(list_t *head)
 		list_t *temp = current;
 
 		current = current->next;
+		free(temp->str);
 		free(temp);
 	}
 }
