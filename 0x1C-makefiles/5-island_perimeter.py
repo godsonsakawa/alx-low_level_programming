202ac47106f8d7a0f4f3b2d2948740258ac801e9
godson sakawa <godsonsakawa@gmail.com>
2023-03-30 00:13:32 +0300

    Update Requirment:Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,10 +22,8 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                """Horizontally"""
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                """Vertically"""
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
