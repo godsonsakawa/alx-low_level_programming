ec5c6de0f2f5c37ad384b6f0e936b666d914c910
Godson Sakawa <godsonsakawa@gmail.com>
2023-07-05 01:58:55 +0300

    check

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,15 +9,12 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+        unsigned int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
-	return (dest);
+        while (i != n)
+        {
+                *dest++ = *src++;
+                i++;
+        }
+        return (dest);
 }
