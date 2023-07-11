838fa087bbc778b0fba9409cd5cdb4883803efb4
Godson Sakawa <godsonsakawa@gmail.com>
2023-07-11 04:35:58 +0300

    Updat e code to error check

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,20 +5,25 @@
   * main - check the code.
   * @argc: refers to count of argumens on the terminal.
   * @argv: refers to an array of srings
-  * Return: always 0 (success)
+  * Return: always 0 (success), -1 on error.
   */
 int main(int argc, char *argv[])
 {
 	int mul;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int num1;
+	int num2;
 
-	mul = (num1 * num2);
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	mul = (num1 * num2);
+
 	printf("%d\n", mul);
 	return (0);
 }
