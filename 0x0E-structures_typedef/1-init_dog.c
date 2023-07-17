1a14a832717b26b47849f36284610258100d7f0c
Godson Sakawa <godsonsakawa@gmail.com>
2023-07-17 17:07:44 +0300

    Strcut project complete

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+  * init_dog - a function that initializes a variable of type struct dog
+  * @name: pointer to name of the dog
+  * @age: pointer to age of the dog
+  * @owner: pointer to the owner of the dog
+  * @d: poiter to type My_dog
+  * Return: void.
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,72 @@
+#include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+  * _strlen - Length of a string.
+  * @str: string to be measured.
+  * Return: Length of the string.
+  */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+	return (len);
+}
+
+/**
+  * _strcopy - copies a string pointed to by src,
+  *            including the terminating null byte,
+  *            to a buffer pointed to by dest.
+  * @dest: The buffer storing the string.
+  * @src: The source string.
+  * Return: Pointer to the destination.
+  */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+  * new_dog - Creates a new dog
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: Owner of the dog
+  * Return: pointer to the new struct dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->name = _strcopy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = _strcopy(new_dog->owner, owner);
+
+	return (new_dog);
+}
