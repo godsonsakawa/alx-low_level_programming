0e7499e8fe5ae6379dd4f17d572b82ba0f7260d8
Godson Sakawa <godsonsakawa@gmail.com>
2023-09-01 03:27:17 +0300

    print binary repr of a number

---
+++ 0x14-bit_manipulation/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - chck the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_binary(0);
+	printf("\n");
+	print_binary(98);
+	printf("\n");
+	return (0);
+}
