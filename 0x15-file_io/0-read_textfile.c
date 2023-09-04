a9166a86c813bb963d0481c71c318ee62988670f
Godson Sakawa <godsonsakawa@gmail.com>
2023-09-04 23:43:50 +0300

    review

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+  * read_textfile - reads a text file and prints it into the POSIX stdout
+  * @filename: a pointer to the file to be read.
+  * @letters: number of letters to be read and printed.
+  * Return: if file cannot be read/ NULL/ write fails - 0
+  *         Otherwise the actual number of letters it could read and print.
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
