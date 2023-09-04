d5c4db7ae13a428788a5b7b33d9aef7df881e60e
Godson Sakawa <godsonsakawa@gmail.com>
2023-09-04 23:43:28 +0300

    header file with prototypes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif /* _ MAIN_H */
